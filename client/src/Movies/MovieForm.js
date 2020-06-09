f9349a589b6ef6fea2e869f998e908033bf71a46
jasonbringle <jasonbringle@gmail.com>
2020-06-08 22:06:01 -0700

    Form is working properly but .Put is not.

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import { FormDiv, FormHolder } from '../Styled'
+import React, { useState } from 'react';
+import { FormDiv, FormHolder } from '../Styled';
+import axios from 'axios';
 
 export default function MovieForm(){
+    const [ updatedMovie, setUpdatedMovie ] = useState({
+        title: "",
+        director: "",
+        metascore: '',
+        stars: []
+    })
+
+    const handleChange = e =>  {
+        setUpdatedMovie({
+            ...updatedMovie,
+            [e.target.name]: e.target.value
+            }
+        )
+    }
+
+    const submitMovieEdit = e => {
+        e.preventDefault();
+            axios
+            .put(`http://localhost/5000/api/movies/${updatedMovie.id}`, updatedMovie)
+            .then(res => console.log(res))
+            .catch(err => console.error(err.message, err.response))
+    }
+
     return(
         <FormHolder>
-            <FormDiv>
-                <input placeholder="Movie Title"/>
-                <input placeholder="Director"/>
-                <input placeholder="Metascore"/> 
+            <FormDiv >
+                <input onChange={handleChange} name='title' value={updatedMovie.title} placeholder="Movie Title"/>
+                <input onChange={handleChange} name='director' value={updatedMovie.director}placeholder="Director"/>
+                <input onChange={handleChange} name='metascore' value={updatedMovie.metascore}placeholder="Metascore"/> 
+                <button onClick={submitMovieEdit}>Submit Edit</button>
             </FormDiv>
         </FormHolder>
     )
